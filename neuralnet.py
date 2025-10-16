afd79a2fb0d1bd4e7c5d5187782857bff2a62e59
Krish Sapovadia <kcsapovadia@gmail.com>
2025-10-16 18:54:41 -0400

    Added activation functions and some static typing

--- neuralnet.py
+++ neuralnet.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.datasets import load_iris
+import numpy.typing as npt
 from typing import List, Tuple, Callable, Any
 
 iris = load_iris()
@@ -10,18 +11,28 @@ y = np.array(iris.target)
 # Research Dropouts
 # Find how to implement regularization
 
+class SigmoidActivation:
+    def forward(self, inputs: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
+        self.output = 1 / (1 + np.exp(-inputs))
+        return self.output
+
+class ReluActivation:
+    def forward(self, inputs: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
+        self.output = np.maximum(0, inputs)
+        return self.output
+
 class Layer:
     def __init__(self, input_size: int, output_size: int):
         self.weights = np.random.randn(input_size, output_size) * np.sqrt(2.0 / input_size)
         self.biases = np.zeroes((1, output_size))
 
-    def forward(self, inputs):
+    def forward(self, inputs: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
         self.inputs = inputs
         self.output = inputs @ self.weights + self.biases
 
         return self.output
     
-    def backward(self, grad_output):
+    def backward(self, grad_output: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
         self.grad_weights = self.inputs.T @ grad_output
         self.grad_biases = np.sum(grad_output, axis=0, keepdims=True)
         grad_input = np.dot(grad_output, self.weights.T)
@@ -32,3 +43,7 @@ class OutputLayer(Layer):
     def __init__(self, input_size: int, output_size: int):
         self.weights = np.random.randn(input_size, output_size) * np.sqrt(1.0 / input_size)
         self.biases = np.zeroes((1, output_size))
+
+hi1 = ReluActivation()
+hi = SigmoidActivation()
+print(hi.forward(hi1.forward(np.array([1,-2,3,-4]))))
