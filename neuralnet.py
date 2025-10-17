82ca939fb66fea50cd54e278aa2789f8a86c8414
Krish Sapovadia <kcsapovadia@gmail.com>
2025-10-16 20:59:18 -0400

    Finished network and added some backprop things

--- neuralnet.py
+++ neuralnet.py
@@ -15,11 +15,20 @@ class SigmoidActivation:
     def forward(self, inputs: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
         self.output = 1 / (1 + np.exp(-inputs))
         return self.output
+    
+    def backward(self, grad_output: float) -> float:
+        # Derivative: sigmoid(x) * (1 - sigmoid(x))
+        sigmoid_derivative = self.output * (1 - self.output)
+        return grad_output * sigmoid_derivative
+
 
 class ReluActivation:
     def forward(self, inputs: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
         self.output = np.maximum(0, inputs)
         return self.output
+    
+    def backward(self, grad_output: float):
+        return grad_output * (self.inputs > 0)
     
 class BCELoss:
     def calculate_fwd(self, predictions: npt.NDArray[np.float64], actuals: npt.NDArray[np.float64]) -> float:
@@ -29,12 +38,10 @@ class BCELoss:
         loss = -np.mean(actuals * np.log(predictions) + (1 - actuals) * np.log(1 - predictions))
         return loss
     
-    def calculate_back(self, predictions: npt.NDArray[np.float64], actuals: npt.NDArray[np.float64]) -> float:
-        self.predictions = predictions
-        self.actuals = actuals
+    def calculate_back(self) -> float:
         n = len(self.predictions)
 
-        gradient = -(actuals / predictions - (1 - actuals) / (1 - predictions)) / n
+        gradient = -(self.actuals / self.predictions - (1 - self.actuals) / (1 - self.predictions)) / n
         return gradient
 
 class SDGOptimizer:
@@ -46,7 +53,7 @@ class SDGOptimizer:
         layer.biases -= self.lr * layer.grad_weights
 
 class Layer:
-    def __init__(self, input_size: int, output_size: int):
+    def __init__(self, *, input_size: int, output_size: int):
         self.weights = np.random.randn(input_size, output_size) * np.sqrt(2.0 / input_size)
         self.biases = np.zeroes((1, output_size))
 
@@ -67,3 +74,49 @@ class OutputLayer(Layer):
     def __init__(self, input_size: int, output_size: int):
         self.weights = np.random.randn(input_size, output_size) * np.sqrt(1.0 / input_size)
         self.biases = np.zeroes((1, output_size))
+
+class NeuralNetwork():
+    def __init__(self):
+        pass
+
+if __name__ == '__main__':
+    X = np.array([
+        [0,0],
+        [1,0],
+        [0,1],
+        [1,1]
+    ])
+
+    y = np.array([
+        [1],
+        [0],
+        [0],
+        [1]
+    ])
+
+    l1 = Layer(input_size=2, output_size=2)
+    l2 = Layer(input_size=2, output_size=1)
+
+    relu = ReluActivation()
+    sigmoid = SigmoidActivation()
+
+    loss_fn = BCELoss()
+    optimizer = SDGOptimizer()
+
+    z1 = l1.forward(X)
+    a1 = relu.forward(z1)
+    z2 = l1.forward(a1)
+    a2 = sigmoid.forward(z2)
+
+    loss = loss_fn.calculate_fwd(a2, y)
+    print(f'Loss: {loss}')
+
+    # Backprop
+    grad = loss_fn.calculate_back()
+    grad = sigmoid.backward(grad)
+    grad = l2.backward(grad)
+    grad = relu.backward(grad)
+    grad = l1.backward(grad)
+
+    optimizer.update(l2)
+    optimizer.update(l1)
